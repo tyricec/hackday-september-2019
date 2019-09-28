1f9c8911dc74789a52ebfc009250c6903072047f
Tyrice Clark <tyricec@civicresource.com>
2019-09-28 16:40:18 -0700

    Change to octonode

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const Octokit = require("@octokit/rest");
-const octokit = new Octokit();
 
-// Compare: https://developer.github.com/v3/repos/#list-organization-repositories
-octokit.repos
-  .listForOrg({
-    org: "octokit",
-    type: "public"
-  })
-  .then(({ data }) => {
-    // handle data
-    console.log(data);
-  });
+var github = require('octonode');
+var client = github.client('a5366b3ba77c861e13ddce4a18f68b1b3f3b2fe0');
 
+// Then we instantiate a client with or without a token (as show in a later section)
+
+var ghme = client.get('/users/SirHexxus', {}, function (err, status, body, headers) {
+  console.log(body); //json object
+});
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
