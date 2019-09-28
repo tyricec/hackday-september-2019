d9a3dfeccc80ed94584d8f8b509c6c1f0cd75c66
Tyrice Clark <tyricec@civicresource.com>
2019-09-28 16:46:29 -0700

    Update token

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const app = express()
 const port = 3000
 
 var github = require('octonode');
-var client = github.client('a5366b3ba77c861e13ddce4a18f68b1b3f3b2fe0');
+var client = github.client('92b481f935266a77628a2ef581ef724fb00c88f2');
 
 // Then we instantiate a client with or without a token (as show in a later section)
 
-var ghme = client.get('/users/SirHexxus', {}, function (err, status, body, headers) {
+var ghme = client.get('/users/SJT1988', {}, function (err, status, body, headers) {
   console.log(body); //json object
+  console.log(err); //json object
 });
 app.get('/', (req, res) => res.send('Hello World!'))
 
