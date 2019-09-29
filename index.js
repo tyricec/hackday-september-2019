3d908b355c07bce7c8fca84047744fc52f66d866
Rebecca Hom <rebecca.hom001@gmail.com>
2019-09-28 19:00:52 -0700

    removed Octonode from the Express app

--- index.js
+++ index.js
@@ -11,30 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/assets', express.static(path.join(__dirname, './public/assets')));
 
-//  Octonode for retrieving GitHub info
-var github = require('octonode');
-var client = github.client(process.env.GITHUB_TOKEN);
-
-// Then we instantiate a client with or without a token (as show in a later section)
-
-/* var ghme = client.get('/users/SJT1988', {}, function (err, status, body, headers) {
-  console.log(body); //json object
-  console.log(err); //json object
-});
-*/
-
-// client.get('/users/SJT1988/repos', {}, function (err, status, body, headers) {
-//   console.log(body);
-// });
-// client.get('/users/SJT1988/orgs', {}, function (err, status, body) {
-
-//   console.log(body);
-// });
-/*client.get('/:owner/:repo/languages', {}, function (err, status, body) {
-  console.log(body);
-});
-*/
-
 //  MongoDB connection
 const mongoUri = `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PWD}@hackday-shard-00-00-ubd8l.mongodb.net:27017,hackday-shard-00-01-ubd8l.mongodb.net:27017,hackday-shard-00-02-ubd8l.mongodb.net:27017/test?ssl=true&replicaSet=HackDay-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
