9deeaa31df30eb5811f8a155c37d81e2b76c1aaa
SirHexxus <jamesmichaelstacy@gmail.com>
2019-09-28 18:36:08 -0700

    initial data for app.js, and minor id name corrections in index.html

---
+++ app.js
@@ -0,0 +1,42 @@
+/* IDs
+card1 - first username
+cardtext1 - first user info
+
+card2 - second username
+cardtext2 - second user info
+
+card3 - third username
+cardtext3 - third user info
+
+graph-dashboard - where the Graph Dashboard goes
+
+first-repo-name
+first-repo-main-lang
+first-repo-tech
+first-repo-stream
+
+second-repo-name
+second-repo-main-lang
+second-repo-tech
+second-repo-stream
+
+third-repo-name
+third-repo-main-lang
+third-repo-tech
+third-repo-stream
+
+fourth-repo-name
+fourth-repo-main-lang
+fourth-repo-tech
+fourth-repo-stream
+
+fifth-repo-name
+fifth-repo-main-lang
+fifth-repo-tech
+fifth-repo-stream
+
+sixth-repo-name
+sixth-repo-main-lang
+sixth-repo-tech
+sixth-repo-stream
+ */
