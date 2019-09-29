e767921bacd6e402ae40ac907f3fe2e49f8ca4a8
Tyrice Clark <tyricec@civicresource.com>
2019-09-28 17:21:10 -0700

    Add example api calls

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const app = express()
 const port = 3000
 
 var github = require('octonode');
-var client = github.client('92b481f935266a77628a2ef581ef724fb00c88f2');
+var client = github.client('d936733e408e75501abdae0002c47ec7528bf6ad');
 
 // Then we instantiate a client with or without a token (as show in a later section)
 
-var ghme = client.get('/users/SJT1988', {}, function (err, status, body, headers) {
+/* var ghme = client.get('/users/SJT1988', {}, function (err, status, body, headers) {
   console.log(body); //json object
   console.log(err); //json object
+});
+*/
+
+client.get('/users/SJT1988/repos', {}, function (err, status, body, headers) {
+  console.log(body);
+});
+client.get('/users/SJT1988/orgs', {}, function (err, status, body) {
+
+  console.log(body);
 });
 app.get('/', (req, res) => res.send('Hello World!'))
 
